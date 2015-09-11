aa971da5f9cee3b98204517fa98bd84c4fde8532
Shayne Chow <shayne@weplus.cn>
2015-09-11 16:11:18 +0800

    搭建登录界面

--- QQZoneDemo/Classes/Controller/LoginViewController.h
+++ QQZoneDemo/Classes/Controller/LoginViewController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  LoginViewController.h
 //  QQZoneDemo
 //
 //  Created by Xiang on 15/9/11.
@@ -8,8 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface LoginViewController : UIViewController
 
 @end
-
---
+++ QQZoneDemo/Classes/Controller/LoginViewController.m
@@ -0,0 +1,37 @@
+//
+//  LoginViewController.m
+//  QQZoneDemo
+//
+//  Created by Xiang on 15/9/11.
+//  Copyright (c) 2015年 周想. All rights reserved.
+//
+
+#import "LoginViewController.h"
+
+@interface LoginViewController ()
+
+@end
+
+@implementation LoginViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
